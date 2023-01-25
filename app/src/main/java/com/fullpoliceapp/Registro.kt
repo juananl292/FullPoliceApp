5084852e6b3c23ccca9ecbf5e0078a89b597bb21
juananl292 <juananl292@gmail.com>
2023-01-25 21:00:57 +0100

    autentication y firebase

--- app/src/main/java/com/fullpoliceapp/Registro.kt
+++ app/src/main/java/com/fullpoliceapp/Registro.kt
@@ -1,11 +1,31 @@
 package com.fullpoliceapp
 
-import androidx.appcompat.app.AppCompatActivity
+import  androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class Registro : AppCompatActivity() {
+    val botonRegistrar: Button by lazy { findViewById<Button>(R.id.botonRegistrar) }
+    val correo: TextInputEditText by lazy { findViewById<TextInputEditText>(R.id.correo) }
+    val password:TextInputEditText by lazy { findViewById<TextInputEditText>(R.id.contrasenaText) }
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_registro)
+        val analytics:FirebaseAnalytics=FirebaseAnalytics.getInstance(this)
+        val bundle=Bundle()
+        bundle.putString("message","Integracion de firebase completa")
+        analytics.logEvent("Initscreen",bundle)
+        setup()
+    }
+    private fun setup(){
+        title="Autentication"
+        botonRegistrar.setOnClickListener {
+            if(correo.getText().isNotEmpty()  && password.text.isNotEmpty()  ){
+
+            }
+        }
     }
 }
