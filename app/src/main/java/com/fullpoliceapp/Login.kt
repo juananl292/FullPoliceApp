465b3cad89cc48b5aa643590736889efa1d4b63e
juananl292 <juananl292@gmail.com>
2023-01-25 19:59:38 +0100

    pagina Registro y estilos

--- app/src/main/java/com/fullpoliceapp/Login.kt
+++ app/src/main/java/com/fullpoliceapp/Login.kt
@@ -1,11 +1,21 @@
 package com.fullpoliceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_login)
+
+        val botonIniciarSesion:Button=findViewById<Button>(R.id.botonInicioSesion)
+        val botonRegistro:Button=findViewById<Button>(R.id.botonRegistrar)
+
+        botonRegistro.setOnClickListener {
+            val intent:Intent=Intent(this,Registro::class.java)
+            this.startActivity(intent)
+        }
     }
 }
