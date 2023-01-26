23d8ea3a9cad1adbf04fcc4e41ec2f1a781bdda0
juananl292 <juananl292@gmail.com>
2023-01-26 18:50:24 +0100

    terminado layout de menu main

--- app/src/main/java/com/fullpoliceapp/MenuMain.kt
+++ app/src/main/java/com/fullpoliceapp/MenuMain.kt
@@ -2,23 +2,27 @@ package com.fullpoliceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MenuMain : AppCompatActivity() {
     val correo:TextView by lazy {findViewById<TextView>(R.id.nombreCorreo)}
-    val pass:TextView by lazy { findViewById<TextView>(R.id.nombrePass) }
+    val botonRanking:Button by lazy { findViewById<Button>(R.id.botonRanking) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        title="Menú"
         setContentView(R.layout.layout_menu_main)
+
         val bundle:Bundle?=intent.extras
        val emai:String?= bundle?.getString("email")
         val pass:String?=bundle?.getString("contra")
         setup(emai?:"",pass?:"")
+
     }
     private fun setup(email:String,contra:String){
-        title="Inicio"
+
         correo.text=email
-        pass.text=contra
+
 
 
 
